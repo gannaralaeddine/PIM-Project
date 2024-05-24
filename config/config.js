2395d2b877e6044dab22143da1b8aa9deea31721
gannaralaeddine <gannarala@gmail.com>
2024-05-24 10:28:45 +0100

    - Change database - Change URL

--- config/config.js
+++ config/config.js
@@ -2,8 +2,13 @@ module.exports = {
     //URL: "mongodb://localhost:27017/MyGuide"
     // URL: "mongodb+srv://gannaraladatabase:database@myguide.eq0mj.mongodb.net/MyGuide?retryWrites=true&w=majority"
 
-    DB_URL: "mongodb+srv://gannarala:gannaralapassword@pim-project.hukf6w7.mongodb.net/",
-    APP_URL: "http://192.168.1.246:5000/",
-    // APP_URL: "http://192.168.1.18:5000/",
+    // DB_URL: "mongodb+srv://gannarala:gannaralapassword@pim-project.hukf6w7.mongodb.net/",
+
+    DB_URL: "mongodb+srv://gannarala:gannaralapassword@pim-project.hukf6w7.mongodb.net/?retryWrites=true&w=majority&appName=PIM-Project/",
+
+// APP_URL: "http://192.168.1.246:5000/",
+//     APP_URL: "http://192.168.1.18:5000/",
+    APP_URL: "https://pim-project.netlify.app/",
+
     secret: "Aladin53130606"
 }
